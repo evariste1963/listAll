4ae87ffe8eb87de65984187743908f817a019e53
royston allfrey <evariste@hotmail.co.uk>
2026-05-16 09:04:41 +0100

    fix: use CREATE TABLE IF NOT EXISTS to prevent migration errors on app restart

--- src/db/migrations.ts
+++ src/db/migrations.ts
@@ -1,12 +1,12 @@
 export const migrationStatements = [
-  `CREATE TABLE \`list_type\` (
+  `CREATE TABLE IF NOT EXISTS \`list_type\` (
 	\`id\` integer PRIMARY KEY NOT NULL,
 	\`name\` text NOT NULL,
 	\`icon\` text NOT NULL,
 	\`fields_config\` text,
 	\`is_default\` integer DEFAULT false
 );`,
-  `CREATE TABLE \`memo_item\` (
+  `CREATE TABLE IF NOT EXISTS \`memo_item\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`list_id\` integer NOT NULL,
 	\`title\` text NOT NULL,
@@ -15,19 +15,19 @@ export const migrationStatements = [
 	\`order\` integer DEFAULT 0,
 	FOREIGN KEY (\`list_id\`) REFERENCES \`memo_list\`(\`id\`) ON UPDATE no action ON DELETE no action
 );`,
-  `CREATE TABLE \`memo_list\` (
+  `CREATE TABLE IF NOT EXISTS \`memo_list\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`title\` text NOT NULL,
 	\`created_at\` integer NOT NULL
 );`,
-  `CREATE TABLE \`shop_tab\` (
+  `CREATE TABLE IF NOT EXISTS \`shop_tab\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`list_id\` integer NOT NULL,
 	\`name\` text NOT NULL,
 	\`order\` integer DEFAULT 0,
 	FOREIGN KEY (\`list_id\`) REFERENCES \`shopping_list\`(\`id\`) ON UPDATE no action ON DELETE no action
 );`,
-  `CREATE TABLE \`shopping_item\` (
+  `CREATE TABLE IF NOT EXISTS \`shopping_item\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`shop_tab_id\` integer NOT NULL,
 	\`title\` text NOT NULL,
@@ -35,13 +35,13 @@ export const migrationStatements = [
 	\`order\` integer DEFAULT 0,
 	FOREIGN KEY (\`shop_tab_id\`) REFERENCES \`shop_tab\`(\`id\`) ON UPDATE no action ON DELETE no action
 );`,
-  `CREATE TABLE \`shopping_list\` (
+  `CREATE TABLE IF NOT EXISTS \`shopping_list\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`title\` text NOT NULL,
 	\`is_active\` integer DEFAULT true,
 	\`created_at\` integer NOT NULL
 );`,
-  `CREATE TABLE \`todo_item\` (
+  `CREATE TABLE IF NOT EXISTS \`todo_item\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`list_id\` integer NOT NULL,
 	\`title\` text NOT NULL,
@@ -51,12 +51,12 @@ export const migrationStatements = [
 	\`order\` integer DEFAULT 0,
 	FOREIGN KEY (\`list_id\`) REFERENCES \`todo_list\`(\`id\`) ON UPDATE no action ON DELETE no action
 );`,
-  `CREATE TABLE \`todo_list\` (
+  `CREATE TABLE IF NOT EXISTS \`todo_list\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`title\` text NOT NULL,
 	\`created_at\` integer NOT NULL
 );`,
-  `CREATE TABLE \`default_shop\` (
+  `CREATE TABLE IF NOT EXISTS \`default_shop\` (
 	\`id\` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	\`name\` text NOT NULL,
 	\`order\` integer DEFAULT 0
